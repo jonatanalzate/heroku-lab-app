bb7aa2dcbb466e2946802a12b61fce46bc311946
jonatanalzate <jonatanalzate@hotmail.com>
2024-09-08 21:17:03 -0500

    Update app.py to use port 5001

--- app.py
+++ app.py
@@ -14,5 +14,5 @@ def cedula():
         return jsonify({"error": "Por favor envía una cédula válida."}), 400
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5001))
+    app.run(host='0.0.0.0', port=port, debug=True)
