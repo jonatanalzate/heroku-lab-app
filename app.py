83ff417ed99a76c7856500130d65f3b4497a7710
jonatanalzate <jonatanalzate@hotmail.com>
2024-09-08 21:00:12 -0500

    Fix app configuration for Heroku

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify
 
+import os
+
 app = Flask(__name__)
 
 @app.route('/cedula', methods=['POST'])
@@ -12,4 +14,5 @@ def cedula():
         return jsonify({"error": "Por favor envía una cédula válida."}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
